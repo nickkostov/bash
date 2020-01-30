3b1d0f884531aa94ffaff04fd3d57266c2637d87
nemame <nikolaykostov.nk@gmail.com>
2020-01-30 16:29:38 +0200

    first script

---
+++ bash/startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo $TITLE
+echo ""
+echo $RIGHT_NOW
+echo ""
+echo $TIME_STAMP
+echo ""
+system_info()
+ {
+
+             echo ""
+             echo "YOU ARE ON"
+             uname -a
+             echo ""
+             echo "RAM ON THIS MACHINE"
+             echo ""
+             free -h
+             echo ""
+             echo $BASH_VERINFO
+             echo ""
+             
+
+     }
+
+"$(system_info)"
