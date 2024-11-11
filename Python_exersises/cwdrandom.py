82dcf552d0be5c5e1313f87ea4f11c775687369d
Nikolay Kostov <nikolay.yavorov.kostov@gmail.com>
2024-11-11 14:39:24 +0200

    Configuring Portfolio

--- Python_exersises/cwdrandom.py
+++
@@ -1,6 +0,0 @@
-import os
-from pprint import pprint
-cwd = os.getcwd()
-for dir_path, dir_names, file_names in os.walk(cwd):
-    for f in file_names:
-        pprint(f)
--- Python_exersises/for_cycle.py
+++
@@ -1,8 +0,0 @@
-for character in 'hello':
-   print(character)
-
-for i in range(11):
-    print(i)
-
-for i in range(-5):
-    print('Hello, world!')
